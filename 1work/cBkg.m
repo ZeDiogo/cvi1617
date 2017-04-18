d55e06a423c6efd18d44f26e0e8c65f74a74f066
ZeDiogo <zediogooliveira@hotmail.com>
2017-04-18 16:52:29 +0100

    Added usefull labfiles and docs

---
+++ 1work/cBkg.m
@@ -0,0 +1,71 @@
+
+
+clear all, close all, clc
+
+% ------ Exemplo 1 --> Pedestre com Camisa Verde no campus IST --------%
+
+vid = VideoReader('sonofvirgin3.avi');
+nFrame = 40*25;
+step = 20;
+
+vid4D = zeros([vid.Height vid.Width 3 nFrame/step]);
+figure,
+k =1;
+    for i=1 : step : nFrame
+            i
+            img = read(vid,i);
+            vid4D(:,:,:,k)=img;
+            imshow(img); drawnow
+            k = k+1;
+            %pause
+    end
+    
+    bkg = median(vid4D,4);
+    figure, imshow(uint8(bkg));
+ 
+%%
+clear all, close all, clc
+%------------------------------------------------------------------------%
+vid = VideoReader('sonofvirgin3.avi');
+nFrame = 40*25;
+step = 20;
+alfa = 0.1;
+
+img = read(vid,1);
+Bkg = zeros(size(img));
+
+figure, hold on
+k =1;
+    for i=1 : step: nFrame
+            i
+            img = read(vid,i);
+            Y = img;
+            Bkg = alfa * double(Y) + (1-alfa) * double(Bkg);
+            imshow(uint8(Bkg)); drawnow
+    end
+  
+%%
+clear all, close all, clc
+%------------------------------------------------------------------------% 
+path = 'MATERIAL\Ist\'; frameIdComp = 4;
+str = ['%s%.' num2str(frameIdComp) 'd.%s'];
+
+nFrame = 548
+step = 4;
+
+vid4D = zeros([288 360 3 nFrame/step]);
+figure;
+for k = 1 : 1 : nFrame/step
+      k
+      str1 = sprintf(str,path,k,'jpg');
+      img = imread(str1);
+      vid4D(:,:,:,k)=img;
+      imshow(img); drawnow
+      hold off
+end
+bkg = median(vid4D, 4);
+figure; imagesc(uint(bkg));
+
+
+
+
