33f550c02cfc5f56cd6a142dc3943bb036a93ff8
ATLSM <andretavareslopessilvamiguel@gmail.com>
2017-04-29 12:21:38 +0100

    update

--- 1work/functions/main.m
+++ 1work/functions/main.m
@@ -1,3 +1,2 @@
 
-'main'
 detectObject('scene00001.png')
--- labs/lab3/roiExp.m
+++ labs/lab3/roiExp.m
@@ -1,35 +1,35 @@
 
 
-% imgg = imread('veiculoGray.jpg');
-% 
-% imshow(imgg); hold on;
-% N=0; but=1;
-% 
-% 
-% 
-% while (but == 1 | but == 32)
-%     
-%     [ci,li,but]=ginput(1);
-%     but
-%     if but == 1 %add point
-%         N = N+1;
-%         cp(N) = ci;
-%         lp(N) = li;
-%         plot(ci,li,'r.','MarkerSize', 8); drawnow;
-%         if N > 1
-%             plot(cp(:),lp(:),'r.-','MarkerSize', 8); drawnow;
-%         end
-%     end
-%      
-% end
-% 
-% 
-% cp = cp';
-% lp = lp';
-% BW = roipoly(imgg,cp,lp);
-% ImCrp = imgg.*uint8(BW);
-% imagesc(ImCrp); colormap gray;
-% pause
+ imgg = imread('veiculoGray.jpg');
+ 
+ imshow(imgg); hold on;
+ N=0; but=1;
+ 
+ 
+ 
+ while (but == 1 | but == 32)
+     
+     [ci,li,but]=ginput(1);
+     but
+     if but == 1 %add point
+         N = N+1;
+         cp(N) = ci;
+         lp(N) = li;
+         plot(ci,li,'r.','MarkerSize', 8); drawnow;
+         if N > 1
+             plot(cp(:),lp(:),'r.-','MarkerSize', 8); drawnow;
+         end
+     end
+      
+ end
+ 
+ 
+ cp = cp';
+ lp = lp';
+ BW = roipoly(imgg,cp,lp);
+ ImCrp = imgg.*uint8(BW);
+ imagesc(ImCrp); colormap gray;
+ pause
 
 
 
