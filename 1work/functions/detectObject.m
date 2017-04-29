61744b8a056b39ae77ba5bb39f6270816eb59d98
ZeDiogo <zediogooliveira@hotmail.com>
2017-04-29 11:28:33 +0100

    update files

--- 1work/functions/detectObject.m
+++ 1work/functions/detectObject.m
@@ -1,6 +1,22 @@
-function [ output_args ] = detectObject( input_args )
+function Boolean = detectObject(frameName)
+
 %DETECTOBJECT Summary of this function goes here
 %   Detailed explanation goes here
+
+framesPath = './frames/'
+frameName2 = strcat(framesPath, frameName)
+
+
+image = imread(frameName2);
+% Check to make sure that it is grayscale, just in case the user substituted their own image.
+%[rows, columns, numberOfColorChannels] = size(originalImage);
+% Do the conversion using standard book formula
+%originalImage = rgb2gray(originalImage);
+    
+% Display the grayscale image.
+%subplot(3, 3, 1);
+imshow(image)
+
 
 
 end
--- 1work/functions/main.m
+++ 1work/functions/main.m
@@ -1,3 +1,3 @@
 
-
+'main'
 detectObject('scene00001.png')
