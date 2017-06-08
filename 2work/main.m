f5520843b9872295d6a28abeb6b75e0cc79ac581
ZeDiogo <zediogooliveira@hotmail.com>
2017-06-08 13:50:13 +0100

    sair de quimica

--- 2work/main.m
+++ 2work/main.m
@@ -2,11 +2,17 @@ clear all
 close all
 clc
 oldpath = addpath('./frames/');
+
+truePos = 0;
+falsePos = 0;
+falseNeg = 0;
+
 groundTruth = load('2015-04-23-14-04-25_jai_eo.gt.txt');
 baseNum = 1997;
 finalFrame = 2999;
 upLPoint = 0;
 upLPointHistory = [0,0;0,0;0,0];
+
 for frameNumber=2390:finalFrame
 % for frameNumber=2245:2399
 % frameNumber = 2390
@@ -33,5 +39,7 @@ for frameNumber=2390:finalFrame
     upLeftCorner = [ groundTruth(frameNumber-baseNum+1, 2), groundTruth(frameNumber-baseNum+1, 3) ];
     dimension = [ groundTruth(frameNumber-baseNum+1, 4), groundTruth(frameNumber-baseNum+1, 5) ];
     drawBox(img, binaryImage, upLeftCorner, dimension, upLPoint, dWindow, frameNumber)
+    
+    [ truePos, falsePos, falseNeg ] = precRecall(upLeftCorner, dimension, upLPoint, dWindow, truePos, falsePos, falseNeg);
    
 end
--- 2work/precRecall.m
+++ 2work/precRecall.m
@@ -1,9 +1,7 @@
-function [ truePos, falsePos, falseNeg ] = precRecall( upLeftCorner, dimension, upLPoint, dWindow)
+function [ truePos, falsePos, falseNeg ] = precRecall( upLeftCorner, dimension, upLPoint, dWindow, truePos, falsePos, falseNeg )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    truePos = 0;
-    falsePos = 0;
-    falseNeg = 0;
+    
     
     
 
