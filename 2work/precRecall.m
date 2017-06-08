9a3c2abdfada9a2c4cf8cbf8283a3a80320d675e
ATLSM <andretavareslopessilvamiguel@gmail.com>
2017-06-08 13:25:54 +0100

    first statistic function

---
+++ 2work/precRecall.m
@@ -0,0 +1,13 @@
+function [ truePos, falsePos, falseNeg ] = precRecall( upLeftCorner, dimension, upLPoint, dWindow)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    truePos = 0;
+    falsePos = 0;
+    falseNeg = 0;
+    
+    
+
+
+
+end
+
